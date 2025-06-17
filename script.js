7f54124993446722db21c822fb7dfc7470d98f74
achtaA-a <sabounnhala@gmail.com>
2025-06-17 16:34:56 +0100

    modifier une tache

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let inputField =document.getElementById("inputField");
+let inputField = document.getElementById("inputField");
 let tasks = document.getElementById("tasks");
 // console.log(tasks);
 const addTask = () => {
-    let task =inputField.value.trim();
+    let task = inputField.value.trim();
     const li = document.createElement("li");
-    li.innerHTML =`
+    li.innerHTML = `
      <label>
             <input type="checkbox" />
             <span>${task}</span>
@@ -13,13 +13,25 @@ const addTask = () => {
           <span class="deleteBtn" title="Supprimer"><i class="fa-solid fa-trash"></i></span>
     `
     tasks.appendChild(li);
-    inputField.value ="";
+    inputField.value = "";
     //console.log(li);
-const checkbox = li.querySelector("input");
-//console.log(checkbox);
-checkbox.addEventListener("click", () => {
+
+    const checkbox = li.querySelector("input");
     let span1 = li.querySelector("span");
-    span1.classList.toggle("checked");
-    
-})
+    const editBtn = li.querySelector(".editBtn");
+    console.log(span1.textContent);
+
+    //console.log(checkbox);
+    checkbox.addEventListener("click", () => {
+        span1.classList.toggle("checked");
+    })
+
+    editBtn.addEventListener("click", function () { 
+        const taskUpdate = prompt("editer tache:", span1.textContent)
+        if (taskUpdate !== null) {
+            span1.textContent = taskUpdate ;
+            checkbox.checked = false;
+            span1.classList.remove("checked");
+        }
+    })
 };
