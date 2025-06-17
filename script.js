5ada480e60175319e4e6f6443bf8d6ea638f3788
achtaA-a <sabounnhala@gmail.com>
2025-06-17 15:31:33 +0100

    voicimon travail sur le HTML CSS et le JS

---
+++ script.js
@@ -0,0 +1,25 @@
+let inputField =document.getElementById("inputField");
+let tasks = document.getElementById("tasks");
+// console.log(tasks);
+const addTask = () => {
+    let task =inputField.value.trim();
+    const li = document.createElement("li");
+    li.innerHTML =`
+     <label>
+            <input type="checkbox" />
+            <span>${task}</span>
+          </label>
+          <span class="editBtn" title="Modifier"><i class="fa-solid fa-pen-to-square"></i></span>
+          <span class="deleteBtn" title="Supprimer"><i class="fa-solid fa-trash"></i></span>
+    `
+    tasks.appendChild(li);
+    inputField.value ="";
+    //console.log(li);
+const checkbox = li.querySelector("input");
+//console.log(checkbox);
+checkbox.addEventListener("click", () => {
+    let span1 = li.querySelector("span");
+    span1.classList.toggle("checked");
+    
+})
+};
