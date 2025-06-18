589c77acf8f4c7834be750336bf769e473df4460
achtaA-a <sabounnhala@gmail.com>
2025-06-18 16:56:30 +0100

    l'application est déja modifier

--- script.js
+++ script.js
@@ -19,19 +19,46 @@ const addTask = () => {
     const checkbox = li.querySelector("input");
     let span1 = li.querySelector("span");
     const editBtn = li.querySelector(".editBtn");
-    console.log(span1.textContent);
-
+    // console.log(span1.textContent);
+    const deleteBtn = li.querySelector(".deleteBtn");
     //console.log(checkbox);
     checkbox.addEventListener("click", () => {
         span1.classList.toggle("checked");
+        counter();
     })
 
-    editBtn.addEventListener("click", function () { 
+    editBtn.addEventListener("click", function () {
         const taskUpdate = prompt("editer tache:", span1.textContent)
         if (taskUpdate !== null) {
-            span1.textContent = taskUpdate ;
+            span1.textContent = taskUpdate;
             checkbox.checked = false;
             span1.classList.remove("checked");
         }
+        counter();
+    })
+    deleteBtn.addEventListener("click", function () {
+        if (confirm("voulez-vous supprimer cette tache ?")) {
+            li.remove(li);
+        }
     })
 };
+const counter = () => {
+    let completedtasks = document.querySelectorAll(".checked").length;
+    let compteur = document.getElementById("completedCounter");
+    compteur.textContent = completedtasks;
+
+    let span2 = document.querySelectorAll("li > label > span.checked").length;
+    let pind = document.getElementById("pendingCounter");
+     pind .textContent = span2;
+}
+const delet = document.querySelector(".delete");
+
+delet.addEventListener("click", () => {
+    if (confirm("confirmez-nous ? ")) {
+        tasks.innerHTML = "";
+    }
+    counter();
+
+});
+
+counter(); 
